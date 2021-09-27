92b97487e4473b3a678f8922baaaebeac1d456a2
sesp13 <sespinosarte99@gmail.com>
2021-09-26 22:15:35 -0500

    agregar jQuery al proyecto de angular

--- proyecto-angular/README.md
+++ proyecto-angular/README.md
@@ -1,2 +1,3 @@
 # Proyecto de angular 
+En este proyecto construiremos una web para administrar un portafolio profesional
 Recordar construir los modulos de node y usar el backend del curso
--- proyecto-angular/src/app/components/contact/contact.component.ts
+++ proyecto-angular/src/app/components/contact/contact.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+//Jquery
+declare var $: any;
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  
   ngOnInit(): void {
+    $('#logo').click(function () {
+      $('header').css('background', 'green');
+    });
   }
-
 }
